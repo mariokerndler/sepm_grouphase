b675ff28170fe0637d4f0b07d557b5b01b51c9ed
Daniel Nissan <e11810773@student.tuwien.ac.at>
2022-05-10 22:53:01 +0200

    added Tag entity

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Tag.java
@@ -0,0 +1,25 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Tag {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+    @Column(nullable = false, length = 35)
+    private String name;
+
+    public  Tag(){
+
+    }
+    public Tag(String name) {
+        this.name = name;
+    }
+}

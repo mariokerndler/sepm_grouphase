296fb0ec8582007583f7a7757989eb04742fe8c3
Mario Kerndler <e51822845@student.tuwien.ac.at>
2022-05-11 23:42:06 +0200

    Generated 2 new components to display images from artists on frontpage. ref #20

---
+++ frontend/src/app/components/midsection/midsection/midsection.component.ts
@@ -0,0 +1,57 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FakerGeneratorService} from '../../../services/faker-generator.service';
+import {Artist} from '../../../dtos/artist';
+import {Artwork} from '../../../dtos/artwork';
+
+@Component({
+  selector: 'app-midsection',
+  templateUrl: './midsection.component.html',
+  styleUrls: ['./midsection.component.scss']
+})
+export class MidsectionComponent implements OnInit {
+
+  @Input() artistId: number;
+  artworks: Artwork[];
+
+  constructor(
+    private fakerService: FakerGeneratorService
+  ) { }
+
+  ngOnInit(): void {
+    const artist: Artist = this.fetchArtist(this.artistId);
+
+    this.artworks = this.fetchArtworks(artist ? artist.artworkIds : null);
+  }
+
+  private fetchArtist(artistId?: number): Artist {
+    if(!artistId) {
+      return null;
+    } else {
+      this.fakerService.generateFakeArtist(artistId, 1, 3).subscribe({
+        next: (artist) => artist
+      });
+    }
+  }
+
+  private fetchArtworks(artworkIds?: number[]): Artwork[] {
+    let artworks: Artwork[] = [];
+
+    if(!artworkIds) {
+      this.fakerService.generateFakeArtworkByAmount(10).subscribe({
+        next: (fakeArtworks) => {
+          artworks = fakeArtworks;
+        }
+      });
+    } else {
+      for (const id of artworkIds) {
+        this.fakerService.generateFakeArtwork(id).subscribe({
+          next: (artwork) => {
+            artworks.push(artwork);
+          }
+        });
+      }
+    }
+
+    return artworks;
+  }
+}

db3aa13e4d2f02a386e3ec283dec6eaeff0bb889
jrabm <e11911128@student.tuwien.ac.at>
2022-05-14 01:27:48 +0200

    Bugfix: Changed table name from User to ApplicationUser. This was done because H2 uses a table named "User" to store metadata. Adding a table with the same name is not allowed and will cause problems!

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/ApplicationUser.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/ApplicationUser.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
-@Table(name="User")
+@Table(name = "ApplicationUser")
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn(discriminatorType = DiscriminatorType.STRING,
     name = "Usertype")

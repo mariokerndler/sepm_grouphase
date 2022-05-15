36e58b1f9181a63fb249307cb3a558a9c7481235
Miriam Brojatsch <e11913096@student.tuwien.ac.at>
2022-05-15 16:18:25 +0200

    Added Chat and Chatmessage entities ("Message" already taken by class from template - tried deleting but has usages)

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Chat.java
@@ -0,0 +1,28 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Chat {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Artist artist;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private ApplicationUser customer;
+
+    @OneToMany(mappedBy = "chat")
+    private List<Chatmessage> messages;
+}

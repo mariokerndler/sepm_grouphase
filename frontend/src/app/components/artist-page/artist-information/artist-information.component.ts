8fa550639cee138b424fde1d9109d687af48e5cd
Mario Kerndler <e51822845@student.tuwien.ac.at>
2022-05-17 01:11:19 +0200

    Basic artist-information layout and some footer changes.

--- frontend/src/app/components/artist-page/artist-information/artist-information.component.ts
+++ frontend/src/app/components/artist-page/artist-information/artist-information.component.ts
@@ -1,6 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Artist} from '../../../dtos/artist';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
 @Component({
   selector: 'app-artist-information',
   templateUrl: './artist-information.component.html',
@@ -10,9 +17,11 @@ export class ArtistInformationComponent implements OnInit {
 
   @Input() artist: Artist;
 
+  artistProfilePicture: string;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.artistProfilePicture = 'https://picsum.photos/100/100';
   }
-
 }

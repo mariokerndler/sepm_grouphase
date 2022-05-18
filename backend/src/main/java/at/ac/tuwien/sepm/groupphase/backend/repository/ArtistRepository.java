0f90b2453097d3473a531a1bcdbe665aae7e3905
Steve <e11845275@student.tuwien.ac.at>
2022-05-18 11:52:44 +0200

    ApplicationUserEndpointTest testing addUsers

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ArtistRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ArtistRepository.java
@@ -1,7 +1,9 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.ArtistDto;
+import at.ac.tuwien.sepm.groupphase.backend.entity.ApplicationUser;
 import at.ac.tuwien.sepm.groupphase.backend.entity.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository

2837cce46277b6b53f30acea0310920756acc834
Miriam Brojatsch <e11913096@student.tuwien.ac.at>
2022-05-22 14:39:51 +0200

    Added ReferenceRepository

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ProfilePictureRepository.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/repository/ProfilePictureRepository.java
@@ -1,7 +1,7 @@
 package at.ac.tuwien.sepm.groupphase.backend.repository;
 
-import at.ac.tuwien.sepm.groupphase.backend.entity.Image;
+import at.ac.tuwien.sepm.groupphase.backend.entity.ProfilePicture;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProfilePictureRepository extends JpaRepository<Image, Long> {
+public interface ProfilePictureRepository extends JpaRepository<ProfilePicture, Long> {
 }

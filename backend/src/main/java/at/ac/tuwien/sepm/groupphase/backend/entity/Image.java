d63e88ef2f4e55fd2fde9ed97696905e48cd6d29
Miriam Brojatsch <e11913096@student.tuwien.ac.at>
2022-05-22 12:43:44 +0200

    Added image entity class #41

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/entity/Image.java
@@ -0,0 +1,56 @@
+package at.ac.tuwien.sepm.groupphase.backend.entity;
+
+import at.ac.tuwien.sepm.groupphase.backend.utils.FileType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150, unique = true)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private FileType fileType;
+
+    public Image(String imageUrl, FileType fileType) {
+        this.imageUrl = imageUrl;
+        this.fileType = fileType;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+            "id=" + id +
+            ", imageUrl='" + imageUrl + '\'' +
+            ", fileType=" + fileType +
+            '}';
+    }
+
+    @Override
+    public int hashCode() {
+        return 41;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Image other = (Image) obj;
+        return id != null && id.equals(other.getId());
+    }
+}

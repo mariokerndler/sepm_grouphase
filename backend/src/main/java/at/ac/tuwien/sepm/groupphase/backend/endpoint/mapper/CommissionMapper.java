23a790d81418d0741c0e3cf9e8ff2c75d1d0b6e6
Miriam Brojatsch <e11913096@student.tuwien.ac.at>
2022-05-27 00:34:00 +0200

    Extended mapper to simple and detailed DTOs, added mapping annotation (Josef) #61

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/CommissionMapper.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/mapper/CommissionMapper.java
@@ -6,12 +6,23 @@ import at.ac.tuwien.sepm.groupphase.backend.entity.Commission;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {UserMapper.class, ArtistMapper.class})
 public abstract class CommissionMapper {
 
-    public abstract SimpleCommissionDto commissionToCommissionDto(Commission commission);
+    @Mapping(source = "artist.id", target = "artistId")
+    @Mapping(source = "customer.id", target = "customerId")
+    public abstract SimpleCommissionDto commissionToSimpleCommissionDto(Commission commission);
 
-    public abstract Commission commissionDtoToCommission(SimpleCommissionDto commissionDto);
+    @Mapping(source = "artist", target = "artistDto")
+    @Mapping(source = "customer", target = "customerDto")
+    public abstract DetailedCommissionDto commissionToDetailedCommissionDto(Commission commission);
+
+    //TODO: do mapping right
+    public abstract Commission simpleCommissionDtoToCommission(SimpleCommissionDto simpleCommissionDto);
+
+    @Mapping(source = "artistDto", target = "artist")
+    @Mapping(source = "customerDto", target = "customer")
+    public abstract Commission detailedCommissionDtoToCommission(DetailedCommissionDto detailedCommissionDto);
 
     //TODO: see what before or after mappings are needed
 }

7cf01cbbf2512c13944cb913c42eb5920bfaab48
Miriam Brojatsch <e11913096@student.tuwien.ac.at>
2022-05-27 00:27:39 +0200

    Added getAll and findById to commissionService #60

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/CommissionService.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/CommissionService.java
@@ -4,7 +4,13 @@ import at.ac.tuwien.sepm.groupphase.backend.entity.Commission;
 
 public interface CommissionService {
 
+    List<Commission> getAllCommissions();
+
+    Commission findById(Long id);
+
     void saveCommission(Commission c);
+
     void updateCommission(Commission c);
+
     void deleteCommission(Commission c);
 }

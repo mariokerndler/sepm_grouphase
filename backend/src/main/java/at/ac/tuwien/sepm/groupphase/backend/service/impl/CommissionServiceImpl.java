c39022fe58980536cabb4133fa69b6bfa694e885
Miriam Brojatsch <e11913096@student.tuwien.ac.at>
2022-05-27 00:33:22 +0200

    Added getAll and findById to commission service (Josef) #60

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/CommissionServiceImpl.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/impl/CommissionServiceImpl.java
@@ -1,12 +1,16 @@
 package at.ac.tuwien.sepm.groupphase.backend.service.impl;
 
 import at.ac.tuwien.sepm.groupphase.backend.entity.Commission;
+import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
 import at.ac.tuwien.sepm.groupphase.backend.repository.CommissionRepository;
 import at.ac.tuwien.sepm.groupphase.backend.service.CommissionService;
 import at.ac.tuwien.sepm.groupphase.backend.utils.ImageFileManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -20,6 +24,23 @@ public class CommissionServiceImpl implements CommissionService {
     public CommissionServiceImpl(CommissionRepository commissionRepo, ImageFileManager ifm) {
         this.commissionRepo = commissionRepo;
         this.ifm = ifm;
+    }
+
+    @Override
+    public List<Commission> getAllCommissions() {
+        return commissionRepo.findAll();
+    }
+
+    @Override
+    public Commission findById(Long id) {
+        Optional<Commission> commission = commissionRepo.findById(id);
+
+        if (commission.isPresent()) {
+            log.info(commission.toString());
+            return commission.get();
+        } else {
+            throw new NotFoundException(String.format("Could not find Commission with id %s", id));
+        }
     }
 
     //TODO: what about exceptions?

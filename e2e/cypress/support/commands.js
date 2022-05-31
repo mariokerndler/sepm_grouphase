1a5e447e5a9240d8be13b121da03c2d168c59880
Leon Christöfl <leon.christoefl@tuwien.ac.at>
2022-05-31 13:45:42 +0200

    Implemented e2e test for user registration. #78

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -8,4 +8,26 @@ Cypress.Commands.add('loginAdmin', () => {
         cy.get('button[name="submit-button"]').click();
         
     })
+});
+
+Cypress.Commands.add('registerUser', () => {
+    cy.fixture('settings').then(settings => {
+        cy.visit(settings.baseUrl);
+        cy.contains('button', 'Register').click();
+        cy.get('input[name="firstname"]').type(settings.firstName);
+        cy.get('input[name="firstname"]').type(settings.firstName);
+        cy.get('input[name="lastname"]').type(settings.lastName);
+        cy.get('input[name="email"]').type(settings.email);
+        cy.get('input[name="address"]').type(settings.address);
+        cy.get('input[name="username"]').type(settings.username);
+        cy.get('input[name="password"]').type(settings.adminPw);
+        cy.get('input[name="confirm"]').type(settings.adminPw);
+        cy.get('button[name="submit-button"]').click();
+        cy.visit(settings.baseUrl);
+        cy.contains('button', 'Login').click();
+        cy.get('input[name="email"]').type(settings.email);
+        cy.get('input[name="email"]').type(settings.email);
+        cy.get('input[name="password"]').type(settings.adminPw);
+        cy.get('button[name="submit-button"]').click();
+    })
 })

6c2c17467a035008fa22fc00838a6c521ae4ec64
Leon Christöfl <leon.christoefl@tuwien.ac.at>
2022-05-31 20:23:06 +0200

    Commission feed now is connected to the backend, endpoint for commissions changed to /api/v1/commissions #73

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CommissionEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/CommissionEndpoint.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/commission")
+@RequestMapping("api/v1/commissions")
 public class CommissionEndpoint {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
--- frontend/src/app/components/commission/commission-feed/commission-feed.component.ts
+++ frontend/src/app/components/commission/commission-feed/commission-feed.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {CommissionDto} from '../../../dtos/commissionDto';
+import {CommissionService} from '../../../services/commission.service';
 
 @Component({
   selector: 'app-commission-feed',
@@ -7,23 +8,32 @@ import {CommissionDto} from '../../../dtos/commissionDto';
   styleUrls: ['./commission-feed.component.scss']
 })
 export class CommissionFeedComponent implements OnInit {
-
+/*
   commission = {
     id: 1, artistId: null, customerId: 1, title: 'Commission Title',
     instructions: 'This is just a random description which describes some of the information within in the commission ',
     sketchesShown: 0, feedbackSend: 0, comArtworkId: null, feedback: [], price: 300,
     issueDate: new Date(2022, 1, 1),
     deadlineDate: new Date(2022, 3, 1), referenceImageIds: [1, 2, 3]
-  } as CommissionDto;
+  } as CommissionDto; */
 
-  constructor() {
+  commissions: CommissionDto[];
+  hasLoaded = false;
+
+  constructor(private commissionService: CommissionService) {
   }
 
   ngOnInit(): void {
+    this.fetchCommissions();
   }
 
-  numSequence(n: number): Array<number> {
-    return Array(n);
+  private fetchCommissions() {
+    this.commissionService.getAllCommissions().subscribe({
+      next: (loadedCommissions) => {
+        this.commissions = loadedCommissions;
+        this.hasLoaded = true;
+      }
+    });
   }
 
 }

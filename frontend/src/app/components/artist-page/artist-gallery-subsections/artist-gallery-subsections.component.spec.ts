2d9520b722cf4e9b3916ff10f81465f959726368
Leon Christöfl <leon.christoefl@tuwien.ac.at>
2022-06-01 18:46:07 +0200

    Generated gallery subsections, they now filter for artist OR tags at the moment #62

---
+++ frontend/src/app/components/artist-page/artist-gallery-subsections/artist-gallery-subsections.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArtistGallerySubsectionsComponent } from './artist-gallery-subsections.component';
+
+describe('ArtistGallerySubsectionsComponent', () => {
+  let component: ArtistGallerySubsectionsComponent;
+  let fixture: ComponentFixture<ArtistGallerySubsectionsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArtistGallerySubsectionsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArtistGallerySubsectionsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

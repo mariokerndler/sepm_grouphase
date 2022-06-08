544f5c97b5b47de0a356d131c68ac8d25de5dbe9
Leon Christöfl <leon.christoefl@tuwien.ac.at>
2022-06-08 20:37:42 +0200

    Added field for inputting tags in image upload #87

--- frontend/src/app/components/upload/upload.component.ts
+++ frontend/src/app/components/upload/upload.component.ts
@@ -1,11 +1,17 @@
-import {Component, Inject, NgZone, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, Inject, NgZone, OnInit, ViewChild} from '@angular/core';
 import {ArtworkDto, FileType} from '../../dtos/artworkDto';
 import {ArtworkService} from '../../services/artwork.service';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {CdkTextareaAutosize} from '@angular/cdk/text-field';
-import {take} from 'rxjs';
+import {map, Observable, startWith, take} from 'rxjs';
 import {NotificationService} from '../../services/notification/notification.service';
+import {TagDto} from '../../dtos/tagDto';
+import {TagService} from '../../services/tag.service';
+import {MatAutocompleteSelectedEvent} from '@angular/material/autocomplete';
+import {MatChipInputEvent} from '@angular/material/chips';
+import {LayoutComponent} from '../artist-page/artist-page-edit/layoutComponent';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'app-upload',
@@ -15,13 +21,21 @@ import {NotificationService} from '../../services/notification/notification.serv
 export class UploadComponent implements OnInit {
 
   @ViewChild('autosize') autosize: CdkTextareaAutosize;
+  @ViewChild('tagInput') tagInput: ElementRef<HTMLInputElement>;
   uploadForm: FormGroup;
   file: any;
   selectedImage;
 
+  selectedTags: TagDto[] = [];
+  separatorKeysCodes: number[] = [ENTER, COMMA];
+  tagForm = new FormControl();
+  allTags: TagDto[] = [];
+  filteredTags: Observable<TagDto[]>;
+
 
   constructor(
     private artworkService: ArtworkService,
+    private tagService: TagService,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private formBuilder: FormBuilder,
     public dialogRef: MatDialogRef<UploadComponent>,
@@ -31,9 +45,18 @@ export class UploadComponent implements OnInit {
       artworkName: ['', [Validators.required]],
       description: [''],
     });
+    this.filteredTags = this.tagForm.valueChanges.pipe(
+      startWith(null),
+      map((tag: TagDto | null) => (tag ? this.filterTags(tag) : this.allTags.slice()))
+    );
   }
 
   ngOnInit(): void {
+    this.tagService.getAllTags().subscribe(
+      (tags) => {
+        this.allTags = tags;
+      }
+    );
   }
 
   onNoClick(): void {
@@ -111,6 +134,64 @@ export class UploadComponent implements OnInit {
       };
       reader.readAsDataURL(file.target.files[0]);
     }
+  }
+
+  /**
+   * Removes a given tag from the chosen tag list.
+   *
+   * @param tag The tag that will be removed
+   */
+  removeTag(tag: TagDto): void {
+    const index = this.selectedTags.indexOf(tag);
+
+    if (index >= 0) {
+      this.selectedTags.splice(index, 1);
+    }
+  }
+
+  /**
+   * Select a tag (autocomplete function)
+   *
+   * @param event Autocomplete event
+   */
+  selectedTag(event: MatAutocompleteSelectedEvent): void {
+    const value: TagDto = event.option.value;
+    this.selectedTags.push(value);
+    this.tagInput.nativeElement.value = '';
+    this.tagForm.setValue(null);
+  }
+
+  /**
+   * Add a tag via text input
+   *
+   * @param event Add a tag via text input
+   */
+  addTag(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+    if (value) {
+      // Find tag
+      const result = this.allTags.filter(tag => tag.name.includes(value));
+      if (result.length !== 0) {
+        return;
+      } else {
+        this.selectedTags.push(result[0]);
+      }
+    }
+
+    event.chipInput?.clear();
+
+    this.tagForm.setValue(null);
+  }
+
+  private filterTags(value: TagDto): TagDto[] {
+    let filterValue;
+    if (typeof value == 'string') {
+      filterValue = String(value).toLowerCase();
+    } else {
+      filterValue = value.name.toLowerCase();
+    }
+
+    return this.allTags.filter(tag => tag.name.toLowerCase().includes(filterValue));
   }
 
 

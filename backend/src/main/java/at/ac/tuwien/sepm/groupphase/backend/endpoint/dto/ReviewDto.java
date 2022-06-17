108e37aeffec562080f9708b5d0a7fc2e27a658b
jrabm <e11911128@student.tuwien.ac.at>
2022-06-17 17:52:34 +0200

    Improved validation for User and ArtistDtos by allowing spaces between names. Also reformatted other dto classes  #102

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/ReviewDto.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/dto/ReviewDto.java
@@ -4,16 +4,33 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class ReviewDto {
 
     private Long id;
+
+    @Valid
     private ArtistDto artistDto;
+
+    @Valid
     private ApplicationUserDto customerDto;
+
+    @Size(max = 255)
     private String text;
+
+    @NotNull
     private Long commissionId;
+
+    @Min(0)
+    @Max(5)
     private int starRating;
 
     public ReviewDto(ArtistDto artistDto, ApplicationUserDto customerDto, String text, Long commissionId, int starRating) {
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/utils/constraints/ValidAlphaNumericWithSpaces.java
@@ -0,0 +1,27 @@
+package at.ac.tuwien.sepm.groupphase.backend.utils.constraints;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@NotBlank
+@Pattern(regexp = "[a-zA-Z0-9 ]*", message = "may only contain numbers, digits or spaces")
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER})
+@Retention(RUNTIME)
+@Documented
+@Constraint(validatedBy = {})
+public @interface ValidAlphaNumericWithSpaces {
+
+    String message() default "field should not be empty and only contain alphanumeric character(s) or spaces.";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}

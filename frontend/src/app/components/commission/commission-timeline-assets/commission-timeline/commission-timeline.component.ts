8c926ac905fc631866539e9a569fbcdc2b6e6327
Leon Christöfl <leon.christoefl@tuwien.ac.at>
2022-06-19 20:28:01 +0200

    Changed some of the coloring #92

--- frontend/src/app/components/commission/commission-timeline-assets/commission-timeline/commission-timeline.component.ts
+++ frontend/src/app/components/commission/commission-timeline-assets/commission-timeline/commission-timeline.component.ts
@@ -1,5 +1,9 @@
 import {ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation} from '@angular/core';
-import * as data from 'src/assets/commission2.json';
+
+//import * as data from 'src/assets/commission2.json';
+import {CommissionService} from '../../../../services/commission.service';
+import {ActivatedRoute} from '@angular/router';
+import {CommissionDto} from '../../../../dtos/commissionDto';
 
 @Component({
   selector: 'app-commission-timeline',
@@ -9,13 +13,29 @@ import * as data from 'src/assets/commission2.json';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CommissionTimelineComponent implements OnInit {
-  data;
+  data;//: CommissionDto;
   isPlaying = false;
-  constructor() { }
+  isReady = false;
+  newData: CommissionDto;
+
+  constructor(private commissionService: CommissionService, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.data = data;
-    console.log(this.data);
+    //this.data = data;
+    this.getCommission();
+  }
+
+  getCommission(){
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.commissionService.getCommissionById(id)
+      .subscribe(
+        (commission) => {
+          //this.data = commission;
+          this.newData = commission;
+          console.log(this.newData);
+          this.isReady = true;
+        }
+      );
   }
 
 }

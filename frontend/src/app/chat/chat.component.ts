653a4c13a4d5361e14dfe3d94527e81b37926bf5
Daniel Nissan <e11810773@student.tuwien.ac.at>
2022-06-22 11:05:01 +0200

    Chat now refreshes, implemented frontend polling that triggers onMessageReceivedEvent #119

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -14,6 +14,8 @@ export class ChatComponent implements OnInit {
 
   public userId;
   public adapter: ChatAdapter;
+  public messages: any[] = [];
+  oldMessages = [];
 
   constructor(private chatService: ChatService) {
 
@@ -29,6 +31,38 @@ export class ChatComponent implements OnInit {
   ngOnInit(): void {
     this.getUserId();
     this.adapter = new Adapter(this.chatService, this.userId);
+    this.chatService.chatListWrapper(this.userId).subscribe(p => {
+      p.forEach(part => {
+        this.chatService.chatHistoryMapper(this.userId, part.participant.id).subscribe(hist => {
+          this.oldMessages.push(hist);
+        });
+      });
+    });
+    const runAsync = () => {
+      this.adapter.onFriendsListChanged(this.userId);
+      setTimeout(() => {
+        const v = runAsync();
+        this.chatService.chatListWrapper(this.userId).subscribe(p => {
+          const newMessages = [];
+          p.forEach(part => {
+            this.chatService.chatHistoryMapper(this.userId, part.participant.id).subscribe(hist => {
+
+              newMessages.push(hist);
+              if (hist.length > this.oldMessages[p.indexOf(part)].length) {
+                if (hist[hist.length - 1].fromId !== Number.parseInt(this.userId, 10)) {
+                  this.adapter.onMessageReceived(p.filter(c => c.participant.id === hist[hist.length - 1].fromId)[0].participant,
+                    hist[hist.length - 1]);
+                }
+              }
+              this.oldMessages[p.indexOf(part)] = hist;
+            });
+          });
+        });
+      }, 1500);
+      return;
+    };
+    runAsync();
   }
+
 
 }

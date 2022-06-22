fc0d318f06d3e9d650f01cf160b694b584c9209c
jrabm <e11911128@student.tuwien.ac.at>
2022-06-23 00:58:57 +0200

    Added use of web hooks to process commission whenever user has paid. Also started work on fulfilling orders by creating a Payment service class. #121

--- backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/PaymentEndpoint.java
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/endpoint/PaymentEndpoint.java
@@ -1,24 +1,31 @@
 package at.ac.tuwien.sepm.groupphase.backend.endpoint;
 
 import at.ac.tuwien.sepm.groupphase.backend.endpoint.dto.CheckoutPaymentDto;
-import com.stripe.exception.StripeException;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.stripe.Stripe;
+import com.stripe.exception.SignatureVerificationException;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Event;
+import com.stripe.model.checkout.Session;
+import com.stripe.net.Webhook;
+import com.stripe.param.checkout.SessionCreateParams;
+import io.micrometer.core.instrument.util.IOUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.annotation.security.PermitAll;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/v1/payments")
-public class StripeEndpoint {
+public class PaymentEndpoint {
+
+    // TODO: Change this to real endpoint key after finishing to test with cli
+    private static final String endpointSecret = "whsec_bb6e0c9d52dcbe770935f0618a4aabc30223c2a3c3bde5273510edb2dbb5fc2b";
 
     /**
      * Payment with Stripe checkout page.
@@ -54,6 +61,41 @@ public class StripeEndpoint {
         responseData.put("id", session.getId());
         // We return the sessionId as a String
         return responseData;
+    }
+
+    /**
+     * Payment with Stripe checkout page.
+     *
+     * @throws StripeException Error that is thrown in case payment fails.
+     */
+    @PermitAll
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/webhook")
+    @Operation(summary = "Receive information from webhook after user finishes payment")
+    public void receiveWebhook(HttpServletRequest request) throws StripeException, IOException {
+        String payload = IOUtils.toString(request.getInputStream());
+        String sigHeader = request.getHeader("Stripe-Signature");
+        Event event = null;
+
+        if (sigHeader == null) {
+            // TODO: Think of appropriate error handling for stripe backend
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Stripe-Signature Header is missing!");
+        }
+
+        try {
+            event = Webhook.constructEvent(payload, sigHeader, endpointSecret);
+        } catch (SignatureVerificationException e) {
+            // Invalid signature
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+
+        // Handle the checkout.session.completed event
+        /*if ("checkout.session.completed".equals(event.getType())) {
+            Session session = (Session) event.getDataObjectDeserializer().getObject();
+
+            // Fulfill the purchase...
+            fulfillOrder(session);
+        }*/
     }
 
     private static void init() {
---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/service/PaymentService.java
@@ -0,0 +1,9 @@
+package at.ac.tuwien.sepm.groupphase.backend.service;
+
+public interface PaymentService {
+
+    /**
+     * Fetches all artists saved in the database and returns them.
+     */
+    void fulfillOrder();
+}

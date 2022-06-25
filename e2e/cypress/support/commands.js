d01d3097776a18409f9fed0e5d881a1987864aa6
Leon Christöfl <leon.christoefl@tuwien.ac.at>
2022-06-25 23:55:50 +0200

    e2e fixing #78

--- e2e/cypress/support/commands.js
+++ e2e/cypress/support/commands.js
@@ -4,8 +4,10 @@ Cypress.Commands.add('loginAdmin', () => {
     cy.fixture('settings').then(settings => {
         cy.visit(settings.baseUrl);
         cy.contains('button', 'Login').click();
+        cy.get('input[name="email"]').click();
+        cy.get('input[name="email"]').click();
         cy.get('input[name="email"]').type(settings.adminUser);
-        cy.get('input[name="email"]').type(settings.adminUser);
+        cy.get('input[name="password"]').click();
         cy.get('input[name="password"]').type(settings.adminPw);
         cy.get('button[name="submit-button"]').click();
         
@@ -19,7 +21,9 @@ Cypress.Commands.add('registerUser', () => {
         cy.get('input[name="firstname"]').type(settings.firstName);
         cy.get('input[name="firstname"]').type(settings.firstName);
         cy.get('input[name="lastname"]').type(settings.lastName);
+        cy.get('input[name="email"]').click();
         cy.get('input[name="email"]').type(settings.email);
+        cy.get('input[name="address"]').click();
         cy.get('input[name="address"]').type(settings.address);
         cy.get('input[name="username"]').type(settings.username);
         cy.get('input[name="password"]').type(settings.adminPw);

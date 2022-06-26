a4374b68a39bf30a9fe1312a7649476e771a3f7a
jrabm <e11911128@student.tuwien.ac.at>
2022-06-26 15:52:35 +0200

    Validated that email of user is not in use yet. Added appropriate Validation message and created annotation #147

---
+++ backend/src/main/java/at/ac/tuwien/sepm/groupphase/backend/utils/constraints/UniqueEmailValidator.java
@@ -0,0 +1,30 @@
+package at.ac.tuwien.sepm.groupphase.backend.utils.constraints;
+
+import at.ac.tuwien.sepm.groupphase.backend.exception.NotFoundException;
+import at.ac.tuwien.sepm.groupphase.backend.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Slf4j
+@Component
+public class UniqueEmailValidator implements ConstraintValidator<UniqueEmail, String> {
+
+    private final UserService userService;
+
+    public UniqueEmailValidator(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public boolean isValid(String value, ConstraintValidatorContext context) {
+        try {
+            userService.findUserByEmail(value);
+            return false;
+        } catch (NotFoundException exception) {
+            return true;
+        }
+    }
+}
